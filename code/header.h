0b0b97f75b00c7252624fd26cc13cd2df5b13fcc
MartinOpat <martinopat.opat@gmail.com>
2023-11-10 22:43:26 +0100

    Updated header.cpp

--- code/header.h
+++ code/header.h
@@ -3,12 +3,17 @@
 using namespace std;
 
 #define ll long long
+#define ull unsigned ll
+#define ld long double
 #define pl pair<ll, ll>
 #define pi pair<int, int>
-#define vl vector<ll>
+#define vll vector<ll>
 #define vi vector<int>
+#define vvi vector<vi>
+#define vvl vector<vl>
 #define vpl vector<pl>
 #define vpi vector<pi>
+#define vld vector<ld>
 #define in(el, cont) (cont.find(el) != cont.end())
 
 constexpr int INF  = 2000000010;
@@ -17,9 +22,11 @@ constexpr ll LLINF = 9000000000000000010LL;
 template <typename T, template <typename ELEM, typename ALLOC = std::allocator<ELEM> > class Container>
 std::ostream& operator<<(std::ostream& o, const Container<T>& container) {
 	typename Container<T>::const_iterator beg = container.begin();
-    o << *beg++;
-	while (beg != container.end()) {
-		o << " " << *beg++;
+	if (beg != container.end()) {
+		o << *beg++;
+		while (beg != container.end()) {
+			o << " " << *beg++;
+		}
 	}
 	return o;
 }
