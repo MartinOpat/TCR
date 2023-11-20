daeaefb1fbced5cd3fabf49c5fbd50481ca34edd
Fabian1907 <78416012+Fabian1907@users.noreply.github.com>
2023-11-20 15:45:13 +0100

    Create tree_node_count.cpp

---
+++ TCR-master/snippets/graphs/tree_node_count.cpp
@@ -0,0 +1,15 @@
+// calculate amount of nodes in each node's subtree
+
+const int mxN = 2e5+5;
+int n, cnt[mxN];
+vector<int> adj[mxN];
+
+void dfs(int s=0, int e=-1)
+{
+    cnt[s] = 1;               // count leaves as one
+    for (int u : adj[s])
+    {
+        dfs(u, s);
+        cnt[s] += cnt[u];   // add up nodes of the subtrees
+    }
+}
