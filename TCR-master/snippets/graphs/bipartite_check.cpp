060bbad9b543b439277bc13bd253a978146369c6
Fabian1907 <78416012+Fabian1907@users.noreply.github.com>
2023-11-23 15:28:33 +0100

    Create bipartite_check.cpp

---
+++ TCR-master/snippets/graphs/bipartite_check.cpp
@@ -0,0 +1,27 @@
+int main()
+{
+    int n;
+    vector<vector<int>> adj(n);
+
+    vector<int> side(n, -1);    // will have 0's for one side 1's for other side
+    bool is_bipartite = true;   // becomes false if not bipartite
+    queue<int> q;
+    for (int st = 0; st < n; ++st) {
+        if (side[st] == -1) {
+            q.push(st);
+            side[st] = 0;
+            while (!q.empty()) {
+                int v = q.front();
+                q.pop();
+                for (int u : adj[v]) {
+                    if (side[u] == -1) {
+                        side[u] = side[v] ^ 1;
+                        q.push(u);
+                    } else {
+                        is_bipartite &= side[u] != side[v];
+                    }
+                }
+            }
+        }
+    }
+}
