06d5b10f75886c1705fad220552bdb6228763a45
MartinOpat <martinopat.opat@gmail.com>
2023-11-24 15:31:05 +0100

    Added pascal's trinagle

---
+++ code/Other Math/pascal_triangle.cpp
@@ -0,0 +1,13 @@
+#include "header.h"
+void printPascal(int n) {
+    for (int line = 1; line <= n; line++) {
+        int C = 1; // used to represent C(line, i)
+        for (int i = 1; i <= line; i++) {
+ 
+            // The first value in a line is always 1
+            cout << C << " ";
+            C = C * (line - i) / i;
+        }
+        cout << "\n";
+    }
+}
