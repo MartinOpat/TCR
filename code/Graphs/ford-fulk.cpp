17b63605c936377ce468e1ad4b81504043112bce
MartinOpat <martinopat.opat@gmail.com>
2023-11-24 15:07:37 +0100

    Added ford fulkerson

---
+++ code/Graphs/ford-fulk.cpp
@@ -0,0 +1,63 @@
+#include "header.h"
+#define V 6  // Num. of vertices in given graph
+
+/* Returns true if there is a path from source 's' to sink
+'t' in residual graph. Also fills parent[] to store the
+path */
+bool bfs(int rGraph[V][V], int s, int t, int parent[]) {
+	bool visited[V];
+	memset(visited, 0, sizeof(visited));
+	queue<int> q;
+	q.push(s);
+	visited[s] = true;
+	parent[s] = -1;
+
+	// Standard BFS Loop
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+
+		for (int v = 0; v < V; v++) {
+			if (visited[v] == false && rGraph[u][v] > 0) {
+				if (v == t) {
+					parent[v] = u;
+					return true;
+				}
+				q.push(v);
+				parent[v] = u;
+				visited[v] = true;
+			}
+		}
+	}
+	return false;
+}
+
+// Returns the maximum flow from s to t in the given graph
+int fordFulkerson(int graph[V][V], int s, int t) {
+	int u, v;
+	int rGraph[V]
+			[V];
+	for (u = 0; u < V; u++)
+		for (v = 0; v < V; v++)
+			rGraph[u][v] = graph[u][v];
+
+	int parent[V]; // This array is filled by BFS and to
+				// store path
+	int max_flow = 0; // There is no flow initially
+	while (bfs(rGraph, s, t, parent)) {
+		int path_flow = INT_MAX;
+		for (v = t; v != s; v = parent[v]) {
+			u = parent[v];
+			path_flow = min(path_flow, rGraph[u][v]);
+		}
+
+		for (v = t; v != s; v = parent[v]) {
+			u = parent[v];
+			rGraph[u][v] -= path_flow;
+			rGraph[v][u] += path_flow;
+		}
+		max_flow += path_flow;
+	}
+	return max_flow;
+}
+
