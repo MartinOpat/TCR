b70bb8403fda784e08255eeac3ec6cd2a53ebe09
Martin <martinopat.opat@gmail.com>
2024-09-20 16:54:10 +0200

    Added daniels algorithms:

---
+++ code/Daniel/kruskal_new.py
@@ -0,0 +1,37 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [-1]*n
+    
+    def find(self, x):
+        if self.parent[x] < 0:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def connect(self, a, b):
+        ra = self.find(a)
+        rb = self.find(b)
+        if ra == rb:
+            return False
+        if self.parent[ra] > self.parent[rb]:
+            self.parent[rb] += self.parent[ra]
+            self.parent[ra] = rb
+        else:
+            self.parent[ra] += self.parent[rb]
+            self.parent[rb] = ra
+        return True
+
+# Full MST is len(spanning==n-1)
+def kruskal(n, edges):
+    uf = UnionFind(n)
+    spanning = []
+    edges.sort(key = lambda d: -d[2])
+    while edges and len(spanning) < n-1:
+        u, v, w = edges.pop()
+        if not uf.connect(u, v):
+            continue
+        spanning.append((u, v, w))
+    return spanning
+
+# Example
+edges = [(1, 2, 10), (2, 3, 20)]
---
+++ code/Daniel/topological_sort.py
@@ -0,0 +1,74 @@
+#Python program to print topological sorting of a DAG
+from collections import defaultdict
+ 
+#Class to represent a graph
+class Graph:
+    def __init__(self,vertices):
+        self.graph = defaultdict(list) #dictionary containing adjacency List
+        self.V = vertices #No. of vertices
+ 
+    # function to add an edge to graph
+    def addEdge(self,u,v):
+        self.graph[u].append(v)
+ 
+    # A recursive function used by topologicalSort
+    def topologicalSortUtil(self,v,visited,stack):
+ 
+        # Mark the current node as visited.
+        visited[v] = True
+ 
+        # Recur for all the vertices adjacent to this vertex
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.topologicalSortUtil(i,visited,stack)
+ 
+        # Push current vertex to stack which stores result
+        stack.insert(0,v)
+ 
+    # The function to do Topological Sort. It uses recursive
+    # topologicalSortUtil()
+    def topologicalSort(self):
+        # Mark all the vertices as not visited
+        visited = [False]*self.V
+        stack =[]
+ 
+        # Call the recursive helper function to store Topological
+        # Sort starting from all vertices one by one
+        for i in range(self.V):
+            if visited[i] == False:
+                self.topologicalSortUtil(i,visited,stack)
+ 
+        # Print contents of stack
+        return stack
+
+    def isCyclicUtil(self, v, visited, recStack):
+ 
+        # Mark current node as visited and
+        # adds to recursion stack
+        visited[v] = True
+        recStack[v] = True
+ 
+        # Recur for all neighbours
+        # if any neighbour is visited and in
+        # recStack then graph is cyclic
+        for neighbour in self.graph[v]:
+            if visited[neighbour] == False:
+                if self.isCyclicUtil(neighbour, visited, recStack) == True:
+                    return True
+            elif recStack[neighbour] == True:
+                return True
+ 
+        # The node needs to be popped from
+        # recursion stack before function ends
+        recStack[v] = False
+        return False
+ 
+    # Returns true if graph is cyclic else false
+    def isCyclic(self):
+        visited = [False] * (self.V + 1)
+        recStack = [False] * (self.V + 1)
+        for node in range(self.V):
+            if visited[node] == False:
+                if self.isCyclicUtil(node, visited, recStack) == True:
+                    return True
+        return False
