1182c4ce94725c8e08a1a9596e0bfe6ac2b67aae
Martin <martinopat.opat@gmail.com>
2024-09-24 18:51:32 +0200

    Added fast python i/o

--- code/auxiliary.py
+++ code/auxiliary.py
@@ -11,3 +11,17 @@ while True:
 def smth_memoi(i, j, s):
     # Example in-built cache
     return "sol"
+
+# Fast I
+import io, os
+def fast_io(): 
+    finput = io.BytesIO(os.read(0,
+        os.fstat(0).st_size)).readline
+    s = finput().decode() 
+    return s
+
+# Fast O 
+import sys
+def fast_out(): 
+    n = 5
+    sys.stdout.write(str(n)+"\n") 
