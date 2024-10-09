da9161ac9dcb844917a3379afeafd6e5c6f33d07
Martin Opat <martinopat.opat@gmail.com>
2024-10-09 12:42:14 +0200

    Fixed typo in floyd-warshall

--- code/Graphs/warshall.cpp
+++ code/Graphs/warshall.cpp
@@ -1,10 +1,10 @@
 #include "header.h"
 // g[i][j] = infty if not path from i to j
 // if g[i][i] < 0, i is contained in a negative cycle
-void warshall(vvl g) {
-    for (int i=0; i<g.size(); ++i) { 
-        for (int j=0; j<g.size(); ++j) { 
-            for (int k=0; k<g.size(); ++k) {
-                if (g[i][k] < LLINF and g[k][j] < LLINF and g[i][j] > g[i][k] + g[k][j]) {
+void warshall(vvl& g) {
+    for (int k=0; k<g.size(); ++k) { 
+        for (int i=0; i<g.size(); ++i) { 
+            for (int j=0; j<g.size(); ++j) {
+                if (g[i][k] < LLONG_MAX and g[k][j] < LLONG_MAX and g[i][j] > g[i][k] + g[k][j]) {
                     g[i][j] = g[i][k] + g[k][j];
 }}}}}
