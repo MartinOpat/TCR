d02ded195f04c6b0c2c1511d4c1fc088399a6970
Martin Opat <martinopat.opat@gmail.com>
2024-10-15 09:25:02 +0200

    Updated bellmann ford

--- code/Graphs/bellmannford.cpp
+++ code/Graphs/bellmannford.cpp
@@ -1,29 +1,51 @@
 #include "header.h"
 // Switch vi and vvpi to vl and vvpl if necessary
-void bellmann_ford_extended(vvpi &e, int source, vi &dist, vb &cyc) {
-	dist.assign(e.size(), INF);
-	cyc.assign(e.size(), false); // true when u is in a <0 cycle
-	dist[source] = 0;
-	for (int iter = 0; iter < e.size() - 1; ++iter){
-		bool relax = false;
-		for (int u = 0; u < e.size(); ++u)
-			if (dist[u] == INF) continue;
-			else for (auto &e : e[u])
-				if(dist[u]+e.second < dist[e.first])
-					dist[e.first] = dist[u]+e.second, relax = true;
-		if(!relax) break;
-	}
-	bool ch = true;
-	while (ch) { // keep going untill no more changes
-		ch = false;	// set dist to -INF when in cycle
-		for (int u = 0; u < e.size(); ++u)
-			if (dist[u] == INF) continue;
-			else for (auto &e : e[u])
-				if (dist[e.first] > dist[u] + e.second
-					&& !cyc[e.first]) {
-					dist[e.first] = -INF;
-					ch = true; //return true for cycles
-					cyc[e.first] = true;
-				}
-	}
+void bellmann_ford_extended(vvpi &e, int source, int goal, vi &dist, vb &cyc) {
+    dist.assign(e.size(), INF);
+    cyc.assign(e.size(), false); // true when u is in a <0 cycle
+    dist[source] = 0;
+
+    // Perform n-1 relaxations
+    for (int iter = 0; iter < e.size() - 1; ++iter) {
+        bool relax = false;
+        for (int u = 0; u < e.size(); ++u) {
+            if (dist[u] == INF) continue;
+            for (auto &edge : e[u]) {
+                int v = edge.first, w = edge.second;
+                if (dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    relax = true;
+                }
+            }
+        }
+        if (!relax) break;
+    }
+    // Step to detect any reachable negative cycles
+    for (int u = 0; u < e.size(); ++u) {
+        if (dist[u] == INF) continue;
+        for (auto &edge : e[u]) {
+            int v = edge.first, w = edge.second;
+            if (dist[u] + w < dist[v]) {
+                // If we can still relax, mark the node in the negative cycle
+                dist[v] = -INF;
+                cyc[v] = true;
+            }
+        }
+    }
+    // Propagate neg. cycle detection to all reachable nodes (if necessary)
+    bool change = true;
+    while (change) {
+        change = false;
+        for (int u = 0; u < e.size(); ++u) {
+            if (!cyc[u]) continue;
+            for (auto &edge : e[u]) {
+                int v = edge.first;
+                if (!cyc[v]) {
+                    cyc[v] = true;
+                    dist[v] = -INF;
+                    change = true;
+                }
+            }
+        }
+    }
 }
