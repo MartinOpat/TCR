dfeffaa0afe075443caf05928cd3c1cf1a020dad
el-sambal <AronHardeman@proton.me>
2024-10-25 16:42:28 +0200

    change #include in order st tree

--- code/Data Structures/order_statistics_tree.cpp
+++ code/Data Structures/order_statistics_tree.cpp
@@ -1,15 +1,9 @@
-/**
- * Description: A set (not multiset!) with support for finding the n'th
- * element, and finding the index of an element.
- * To get a map, change \texttt{null\_type}.
- * Time: O(\log N)
- */
-
-#include "../header.h"
+#include <bits/extc++.h> // !!!!
 using namespace __gnu_pbds;
+using namespace std;
 
 template<class T>
-using Tree = tree<T, null_type, less<T>, rb_tree_tag,
+using Tree = tree<T, null_type, less<T>,rb_tree_tag,
     tree_order_statistics_node_update>;
 
 void example() {
